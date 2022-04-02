50dec4cfa162a91b6c036128292a4a1f38638780
lmada19 <102841643+lmada19@users.noreply.github.com>
2022-04-02 19:01:40 -0400

    Create Shape.java

---
+++ Shape.java
@@ -0,0 +1,29 @@
+/*
+
+public class Shape {
+  private int red, green, blue;
+  private int borderRed, borderGreen, borderBlue;
+  private int borderThickness;
+
+  public Shape() {
+    
+  }
+
+  public void Color(int red, int green, int blue) {
+    this.red = red;
+    this.green = green;
+    this.blue = blue;
+  }
+
+  public void borderColor(int borderRed, borderGreen, borderBlue) {
+    this.borderRed = borderRed;
+    this.borderGreen = borderGreen;
+    this.borderBlue = borderBlue;
+  }
+
+  public void setBorder(int borderThickness) {
+    this.borderThickness = borderThickness;
+  }
+}
+
+*/
