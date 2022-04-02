c15d6b9c25f4e393fcefdf5f97afa110ba1cc37d
lmada19 <102841643+lmada19@users.noreply.github.com>
2022-04-02 18:57:47 -0400

    Create AnimationPlayer.java

---
+++ AnimationPlayer.java
@@ -0,0 +1,47 @@
+import java.io.File;  
+import java.io.FileNotFoundException;  
+import java.util.Scanner; 
+
+
+class AnimationPlayer {
+  public void LoadAnimationFromFile(String filename)
+{
+  
+   
+  
+  try {
+      File myObj = new File("animation1.txt");
+      Scanner myReader = new Scanner(myObj);
+     int static counter=0;
+      while (myReader.hasNextLine()) {
+         String data = myReader.nextLine();
+         counter++;
+         if(counter==4)
+         {
+           
+         }
+        System.out.println(data);
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+}
+  public void player()
+{
+
+}
+
+  public void run() {
+  
+  }
+
+
+  
+  AnimationPlayer player = new AnimationPlayer();
+
+    player.LoadAnimationFromFile("animation1.txt");
+   player.run();
+  }
+}
