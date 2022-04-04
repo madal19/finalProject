b981d353e55c90ad729534cb2e3a81a3274fe882
lmada19 <102841643+lmada19@users.noreply.github.com>
2022-04-04 17:35:28 -0400

    Update AnimationPlayer.java

--- AnimationPlayer.java
+++ AnimationPlayer.java
@@ -1,47 +1,19 @@
-import java.io.File;  
-import java.io.FileNotFoundException;  
-import java.util.Scanner; 
+import java.awt.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-
-class AnimationPlayer {
-  public void LoadAnimationFromFile(String filename)
+public class Animation
 {
-  
-   
-  
-  try {
-      File myObj = new File("animation1.txt");
-      Scanner myReader = new Scanner(myObj);
-     int static counter=0;
-      while (myReader.hasNextLine()) {
-         String data = myReader.nextLine();
-         counter++;
-         if(counter==4)
-         {
-           count++;
-         }
-        System.out.println(data);
-      }
-      myReader.close();
-    } catch (FileNotFoundException e) {
-      System.out.println("An error occurred.");
-      e.printStackTrace();
+
+    
+    public static void main(String[] args) {
+        MyFrame f= new MyFrame();
+     
     }
-}
-  public void player()
-{
-
-}
-
-  public void run() {
   
-  }
-
-
-  
-  AnimationPlayer player = new AnimationPlayer();
-
-    player.LoadAnimationFromFile("animation1.txt");
-   player.run();
-  }
+    
 }
+
+
