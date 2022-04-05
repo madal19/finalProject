57b7fefcc3fbd8f64b6d5fcf7e114e0bedbc43b2
lmada19 <102841643+lmada19@users.noreply.github.com>
2022-04-05 05:41:24 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         Circle far= new Circle();
-        far.setr(80);
+        //far.setr(80);
         
         AnimationPlayer f= new AnimationPlayer();
    f.run();
