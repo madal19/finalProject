da6deb8df67f9213e5c07e760404698c3dddd93d
lmada19 <102841643+lmada19@users.noreply.github.com>
2022-04-05 05:30:54 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Circle far= new Circle();
+        far.setr(80);
+        
+        AnimationPlayer f= new AnimationPlayer();
+   f.run();
+}
+}
