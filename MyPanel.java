c57c7e1662a88539bee93c2d719ee9caf7466ab1
lmada19 <102841643+lmada19@users.noreply.github.com>
2022-04-04 23:47:37 -0400

    Update MyPanel.java

--- MyPanel.java
+++ MyPanel.java
@@ -4,21 +4,25 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-a *
+ *
  * @author Arvind
  */
 public class MyPanel extends JPanel implements ActionListener{
-   int PANEL_WIDTH= 100;
-   int PANEL_HEIGHT= 100;
-   Timer timer;
+Timer timer;
+   final int PANEL_WIDTH= 400;
+   final int PANEL_HEIGHT= 300;
    int xVelocity=1;
-   int yVecloxity=1;
+   int yVelocity=1;
    int x=0;
-   int y=0;
-   int r=60;
+   int y=2;
    
+   int xnew=100;//for jump effect
+   int ynew=200;//for jump effect
     
-    
+    //int radius=c[i].getR(); //only for circle
+    int r=50;
+    String choice="circle"; //choice 
+    String Effect="hide";    
     MyPanel(){
         this.setPreferredSize(new Dimension(PANEL_WIDTH,PANEL_HEIGHT));
         this.setBackground(Color.black);
@@ -26,13 +30,32 @@ public class MyPanel extends JPanel implements ActionListener{
         timer.start();
     }
     
-    public void paint(Graphics g){
-        super.paint(g);//backround 
-        Graphics2D g2D= (Graphics2D) g;
-        g2D.setStroke(new BasicStroke(5)); //changes width
-        g2D.setPaint(new Color(0,0,255)); //colour
-          g2D.drawOval(x, y, r*2, r*2); //creates circle
+     public void paint(Graphics g){
+
+      super.paint(g);//backround 
       
+        Graphics2D g2D= (Graphics2D) g;
+        if(choice=="circle"){
+          g2D.setStroke(new BasicStroke(5)); //changes width
+        g2D.setPaint(new Color(0,0,255)); //colour
+     //g2D.setPaint(new Color(0,0,255,0)); //hide
+          //g2D.drawOval(x, y, r*2, r*2); //creates circle
+         g2D.fillOval(x, y, r*2, r*2);
+       
+        }
+        if(choice=="rec"){
+              g2D.setPaint(new Color(0,0,255)); //colour
+             g2D.setStroke(new BasicStroke(4)); //changes width
+             g2D.drawRect(x, y, r, r*2);
+           
+        }
+        if(choice=="line"){
+            g2D.setPaint(new Color(0,0,255)); //colour
+             g2D.setStroke(new BasicStroke(9)); //changes width
+            g2D.drawLine(x, y+10, x+10, y+10);
+        }
+              
+  
              
     }
    @Override
@@ -40,8 +63,16 @@ public class MyPanel extends JPanel implements ActionListener{
        if(x>=PANEL_WIDTH-r*2 || x<0){
           xVelocity= xVelocity * -1;
        }
-        x=x+xVelocity;
-        repaint();
+      x=x+xVelocity;
+        
+      if(Effect=="jump"){ //for the jump effect
+         x=xnew;
+        y=ynew;
+        }
+
+         repaint();
+        
     }
-    
+
 }
+
